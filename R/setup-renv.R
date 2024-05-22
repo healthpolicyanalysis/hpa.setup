5088867f651e990e4ea7d559fb1671ca0295afe8
RWParsons <rex.parsons94@gmail.com>
2024-05-22 12:58:25 +1000

    fix typo

--- R/setup-renv.R
+++ R/setup-renv.R
@@ -10,19 +10,19 @@ setup_renv <- function(path,
 
   # create dependencies file in project dir
   if (!is.null(dependencies_file_path)) {
-    file.copy(dependencies_file_path, file.path(path, "depencies.R"))
+    file.copy(dependencies_file_path, file.path(path, "dependencies.R"))
   }
 
   if (!is.null(dependencies)) {
     for (pkg in dependencies) {
       pkg_line <- glue::glue("library({pkg})")
-      write(pkg_line, file.path(path, "depencies.R"), append = TRUE)
+      write(pkg_line, file.path(path, "dependencies.R"), append = TRUE)
     }
   }
 
   withr::with_dir(path, renv::init(load = FALSE, restart = FALSE))
 
-  file.remove(file.path(path, "depencies.R"))
+  file.remove(file.path(path, "dependencies.R"))
 
   path
 }
